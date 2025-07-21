9966f15ef1997dc83ef483b9ecaf6d013eb8e612
Sabbih <alorferi.pc003@outlook.com>
2025-07-21 12:42:35 +0600

    Update class.constant.php

--- classes/class.constant.php
+++ classes/class.constant.php
@@ -1,73 +1,30 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-// Only define DB constants if not defined yet
-if (!defined('DB_HOST')) {
-    // if ($_SERVER['SERVER_NAME'] === 'localhost') {
-        // Local development DB config
-        define("DB_HOST", "localhost");
-        define("DB_USER", "root");
-        define("DB_PASS", "");
-        define("DB_NAME", "ticker_system"); // your local DB name
-    // } else {
-    //     // Production DB config
-    //     define("DB_HOST", "localhost");
-    //     define("DB_USER", "u833998383_ticket_master");
-    //     define("DB_PASS", "u833998383_Ticket_master");
-    //     define("DB_NAME", "u833998383_ticket_master");
-    // }
-}
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "u833998383_ticket_master");
 
-// Url
-if (!defined('APP_URL')) {
-    define("APP_URL","https://appsghar.com/projects/ticket_master/v1");
-}
+//Url
+define("APP_URL", "https://appsghar.com/projects/ticket_master/v1");
 
-// Paths
-if (!defined('PROFILE_PHOTOS_PATH')) {
-    define("PROFILE_PHOTOS_PATH","/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/events/");
-}
-if (!defined('TEMP_PATH')) {
-    if ($_SERVER['HTTP_HOST'] === 'localhost') {
-        // Local development (XAMPP)
-        define("TEMP_PATH", "C:/xampp12/htdocs/TicketMasterApi/uploads/temp/");
-    } else {
-        // Live server
-        define("TEMP_PATH", "/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/temp/");
-    }
-}
+//Paths
+define("PROFILE_PHOTOS_PATH", "/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/events/");
+define("TEMP_PATH", "/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/temp/");
+
+//Paths for url
+define("PROFILE_PATH_URL", "/uploads/events/");
+
+//Errors Configurations
+define("ERROR_SUCCESS", 0);
+define("ERROR_UNKNOWN", 100);
+define("ERROR_ACCOUNT_ID", 400);
+define("REVOKED_ACCESS", 500);
 
 
-// Paths for url
-if (!defined('PROFILE_PATH_URL')) {
-    define("PROFILE_PATH_URL","/uploads/events/");
-}
-
-// Errors Configurations
-if (!defined('ERROR_SUCCESS')) {
-    define("ERROR_SUCCESS", 0);
-}
-if (!defined('ERROR_UNKNOWN')) {
-    define("ERROR_UNKNOWN", 100);
-}
-if (!defined('ERROR_ACCOUNT_ID')) {
-    define("ERROR_ACCOUNT_ID", 400);
-}
-if (!defined('REVOKED_ACCESS')) {
-    define("REVOKED_ACCESS", 500);
-}
-
-// Email Configurations
-if (!defined('HOST')) {
-    define("HOST", "");
-}
-if (!defined('EMAIL')) {
-    define("EMAIL", "");
-}
-if (!defined('PASSWORD')) {
-    define("PASSWORD", "");
-}
-if (!defined('TITLE')) {
-    define("TITLE", "");
-}
-?>
+//Email Configurations
+define("HOST", "");
+define("EMAIL", "");
+define("PASSWORD", "");
+define("TITLE", "");
