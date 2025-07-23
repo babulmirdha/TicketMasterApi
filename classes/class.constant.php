8c82ca5413cd881d638d577cf8d027e53f3aa35c
Babul Mirdha <babumirdha@gmail.com>
2025-07-23 08:00:56 +0100

    Update class.constant.php

--- classes/class.constant.php
+++ classes/class.constant.php
@@ -4,19 +4,24 @@ header('Content-Type: application/json; charset=utf-8');
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "Toor@8622");
 define("DB_NAME", "u833998383_ticket_master");
 
 //Url
 // define("APP_URL","https://appsghar.com/projects/ticket_master/v1");
-define("APP_URL", "http://192.168.0.140/TicketMasterApi");
+define("APP_URL", "http://192.168.0.245/TicketMasterApi");
 
 //Paths
 // define("PROFILE_PHOTOS_PATH","/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/events/");
 // define("TEMP_PATH","/home/u833998383/domains/appsghar.com/public_html/projects/ticket_master/uploads/temp/");
 
-define("PROFILE_PHOTOS_PATH", "C:/xampp/htdocs/TicketMasterApi/public/uploads/events/");
-define("TEMP_PATH", "C:/xampp/htdocs/TicketMasterApi/public/uploads/temp/");
+
+define("PROFILE_PHOTOS_PATH", "/var/www/html/TicketMasterApi/public/uploads/events/");
+define("TEMP_PATH", "/var/www/html/TicketMasterApi/public/uploads/temp/");
+
+
+//define("PROFILE_PHOTOS_PATH", "C:/xampp/htdocs/TicketMasterApi/public/uploads/events/");
+//define("TEMP_PATH", "C:/xampp/htdocs/TicketMasterApi/public/uploads/temp/");
 
 
 //Paths for url
