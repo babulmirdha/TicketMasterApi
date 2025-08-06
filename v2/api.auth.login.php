24c340fbc7f3965507f96aa69fd39ea114a8cb29
Babul Mirdha <babumirdha@gmail.com>
2025-08-06 20:25:55 +0600

    update

--- v2/api.auth.login.php
+++ v2/api.auth.login.php
@@ -4,7 +4,7 @@ require_once '../classes/class.constant.php';
 require_once '../classes/class.events.php';
 require_once '../classes/class.accounts.php';
 
-if (!empty($_POST)) {
+if (! empty($_POST)) {
 
     $email = isset($_POST['email']) ? $_POST['email'] : '';
 
@@ -13,10 +13,13 @@ if (!empty($_POST)) {
     $account = new accounts();
 
     //Checking if user is already registered
-    if ($account->login($email,$password)) $result = array('error' => False, 'msg' => 'User Login Successfully!',"data"=>$account->getAccountInfoByEmail($email));
-    else  $result = array("error" => True, "msg" => "Incorrect Login Details!");
+    if ($account->login($email, $password)) {
+        $result = ['error' => false, 'msg' => 'User Login Successfully!', "data" => $account->getAccountInfoByEmail($email)];
+    } else {
+        $result = ["error" => true, "msg" => "Incorrect Login Details!"];
+    }
+
     echo json_encode($result);
     exit;
 
 }
-?>
